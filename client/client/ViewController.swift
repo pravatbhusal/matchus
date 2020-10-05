5072087091b52e589a38ec3db84a2a4f518c8086
Pravat Bhusal <pravat.bhusal@gmail.com>
2020-10-05 14:31:36 -0500

    Add client boilerplate

---
+++ client/client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  client
+//
+//  Created by pbhusal on 10/5/20.
+//  Copyright © 2020 MatchUs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
