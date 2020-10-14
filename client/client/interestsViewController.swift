298d1a0d6f924e408b53a9d9834f45f869813de3
Jinho Yoon <yykuk2003@gmail.com>
2020-10-15 04:59:03 +0900

    Added interests view controller

--- client/client/interestsViewController.swift
+++ client/client/interestsViewController.swift
@@ -8,10 +8,49 @@
 
 import UIKit
 
-class interestsViewController: UIViewController {
+public var interests:[String] = []
+
+class interestsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+
+    
+    let textCellIdentifier = "TextCell"
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var interestText: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return interests.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: textCellIdentifier, for: indexPath as IndexPath)
+
+        let row = indexPath.row
+        cell.textLabel?.text = interests[row]
+        return cell
+    }
+
+    
+    @IBAction func xButton(_ sender: Any) {
+        let buttonPosition = (sender as AnyObject).convert(CGPoint.zero, to: self.tableView)
+        let indexPath = self.tableView.indexPathForRow(at:buttonPosition)
+        interests.remove(at: indexPath!.row)
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    @IBAction func addButton(_ sender: Any) {
+        if interests.count < 4 {
+            interests.append(interestText.text!)
+        }
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
 }
