3788f68223bf9954127783db73f99ce4b771c3f0
promazo <promazo@promazos-Mac-Pro.hsd1.in.comcast.net>
2020-10-15 19:08:45 -0400

    add location confirmation

--- client/client/locationViewController.swift
+++ client/client/locationViewController.swift
@@ -7,24 +7,34 @@
 //
 
 import UIKit
+import CoreLocation
 
-class locationViewController: UIViewController {
+class locationViewController: UIViewController, CLLocationManagerDelegate {
 
+    var manager: CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        manager = CLLocationManager()
+        manager?.delegate = self
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        manager?.requestWhenInUseAuthorization()
+        manager?.startUpdatingLocation()
     }
-    */
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let first = locations.first else {
+            return
+        }
+        
+        print(first)
+    }
 
 }
