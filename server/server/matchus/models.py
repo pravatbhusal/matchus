00505fce50a67faebd66c3a8c083b96d78296861
Pravat Bhusal <pravat.bhusal@gmail.com>
2020-10-16 17:08:13 -0500

    Program user signup view and model for user

--- server/server/matchus/models.py
+++ server/server/matchus/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class UserAccount(models.Model):
+    email = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.email
--- server/server/matchus/views.py
+++ server/server/matchus/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import UserAccount
+from .serializers import UserAccountSerializer
 
-# Create your views here.
+class SignupView(APIView):
+    def post(self, request):
+        accounts = UserAccount.objects.all()
+        serializer = UserAccountSerializer(accounts, many=True)
+        return Response(serializer.data)
