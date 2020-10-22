cfde1f79c0f2c2031094ce0de22f941bfffa1722
Pravat Bhusal <pravat.bhusal@gmail.com>
2020-10-22 15:27:48 -0500

    Program table view to be smaller iheight

---
+++ client/client/InterestCell.swift
@@ -0,0 +1,24 @@
+//
+//  InterestCell.swift
+//  
+//
+//  Created by pbhusal on 10/22/20.
+//
+
+import UIKit
+
+class InterestCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+
+}
