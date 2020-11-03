660f0be0859c46b00e730fea58b1b417c81a1b13
Pravat Bhusal <pravat.bhusal@gmail.com>
2020-11-03 14:48:03 -0600

    Allow static files for the API

--- server/server/matchus/serializers.py
+++ server/server/matchus/serializers.py
@@ -4,4 +4,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'name', 'interests', 'profilePhoto', 'photos')
+        fields = ('id', 'name', 'interests', 'profilePhoto')
--- server/server/matchus/urls.py
+++ server/server/matchus/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('verify-credentials/', views.VerifyCredentialsView.as_view()),
-    path('signup/', views.SignUpView.as_view()),
-    path('login/', views.LoginView.as_view()),
-    path('profile/', views.ProfileView.as_view()),
+    path('verify-credentials', views.VerifyCredentialsView.as_view()),
+    path('signup', views.SignUpView.as_view()),
+    path('login', views.LoginView.as_view()),
+    path('profile', views.ProfileView.as_view()),
     path('profile/<int:id>', views.ProfileView.as_view()),
-    path('logout/', views.LogoutView.as_view()),
-]
+    path('profile/profile-photo', views.ProfileView.as_view()),
+    path('profile/photos', views.ProfileView.as_view()),
+    path('logout', views.LogoutView.as_view()),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
