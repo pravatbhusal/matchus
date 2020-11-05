acc9a60b1cb8a8b28d0ac42cf0f7181538ac5255
promazo <promazo@promazos-Mac-Pro.hsd1.in.comcast.net>
2020-11-04 19:31:38 -0500

    initial dashboard code

---
+++ client/client/DashboardViewController.swift
@@ -0,0 +1,82 @@
+//
+//  DashboardViewController.swift
+//  Matchus
+//
+//  Created by Jinho Yoon on 11/1/20.
+//  Copyright © 2020 MatchUs. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class DashboardProfile {
+    var profileId: Int!
+    var name: String!
+    var profilePhoto: String!
+    var photo: String!
+    var profileTag: String!
+}
+
+class DashboardCell: UITableViewCell {
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var profilePhoto: UIImageView!
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var profileTag: UILabel!
+}
+
+class DashboardViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var plusButton: UIButton!
+    
+    var token: String!
+    var profiles:[DashboardProfile]!
+    var pageNum:Int!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        plusButton.layer.cornerRadius = 18
+        token = UserDefaults.standard.string(forKey: User.token)
+        pageNum = 1
+        loadProfiles()
+    }
+    
+    func loadProfiles() {
+        let url = APIs.serverURI + "/dashboard/" + String(pageNum)
+        
+        AF.request(url, method: .get, parameters: nil, headers: ["Authorization": token]).responseJSON { [self]
+         response in
+            switch response.response?.statusCode {
+                    case 200?:
+                     if let json = response.value {
+                        let profilePhotoURL: String = ResponseSerializer.getProfilePicture(json: json)!
+                        let profileName: String = ResponseSerializer.getProfileName(json: json)!
+                        let photoURLs: [String] = ResponseSerializer.getFeaturedPhotoURLs(json: json)!
+                         // populate dashboard array here
+                         
+                         //let dashboardProfiles: [DashboardProfile] = ResponseSerializer.get(json: json)!
+                         //self.chats = chatProfiles
+                         self.tableView.reloadData()
+                     }
+                     break;
+            default:
+                if let json = response.value {
+                    let errorMessage: String? = ResponseSerializer.getErrorMessage(json: json)
+                    print(errorMessage ?? "")
+                }
+                break;
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return profiles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "DashboardCell", for: indexPath as IndexPath) as! DashboardCell
+        let row = indexPath.row
+        
+        return cell
+    }
+}
