2285b73b386c87152dd80c33c97682e2bb54aa97
promazo <promazo@promazos-Mac-Pro.hsd1.in.comcast.net>
2020-11-18 11:41:11 -0500

    keyboard pushes up view when editing bio

--- client/client/Onboarding/IdentityViewController.swift
+++ client/client/Onboarding/IdentityViewController.swift
@@ -28,10 +28,27 @@ class IdentityViewController: UIViewController, UIImagePickerControllerDelegate,
         super.viewDidLoad()
         bioText.layer.borderWidth = 1
         bioText!.layer.borderColor = UIColor.black.cgColor
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
     }
     
     @IBAction func profilePhotoPressed(_ sender: Any) {
